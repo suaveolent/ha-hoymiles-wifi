0c4eef5cb24eb00cd041846faf352c00d10c7562
suaveolent <suaveolent@users.noreply.github.com>
2024-01-11 16:36:10 +0100

    bump version to v.0.0.6

--- custom_components/hoymiles_wifi/const.py
+++ custom_components/hoymiles_wifi/const.py
@@ -2,7 +2,7 @@ DOMAIN = "hoymiles_wifi"
 NAME = "Hoymiles HMS-XXXXW-T2"
 DOMAIN = "hoymiles_wifi"
 DOMAIN_DATA = f"{DOMAIN}_data"
-VERSION = "0.0.5"
+VERSION = "0.0.6"
 
 ISSUE_URL = "https://github.com/suaveolent/ha-hoymiles-wifi/issues"
 

71a8ebcbd3fef0b6304b8de9a2e277d79e825dfb
suaveolent <suaveolent@users.noreply.github.com>
2024-01-30 16:46:14 +0100

    use device info

--- custom_components/hoymiles_wifi/entity.py
+++ custom_components/hoymiles_wifi/entity.py
@@ -2,6 +2,7 @@
 import logging
 
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.entity import Entity, EntityDescription
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
@@ -19,25 +20,20 @@ class HoymilesEntity(Entity):
     def __init__(self, config_entry: ConfigEntry, description: EntityDescription):
         """Initialize the Hoymiles entity."""
         super().__init__()
-        self._config_entry = config_entry
         self.entity_description = description
+        self._config_entry = config_entry
         self._sensor_prefix = f' {config_entry.data.get(CONF_SENSOR_PREFIX)} ' if config_entry.data.get(CONF_SENSOR_PREFIX) else ""
         self._attr_unique_id = f"hoymiles_{config_entry.entry_id}_{description.key}"
 
         self._dtu_sn = ""
 
-
-    @property
-    def device_info(self):
-        """Return device information about the sensor."""
-        return {
-            "identifiers": {(DOMAIN, self._config_entry.entry_id)},
-            "name": "Hoymiles HMS-XXXXW-T2" + self._sensor_prefix,
-            "manufacturer": "Hoymiles",
-            "model": "HMS-XXXXW-T2",
-            "serial_number": self._dtu_sn,
-            "via_device": (DOMAIN, "inverter_state"),
-        }
+        self._attr_device_info = DeviceInfo(
+            identifiers={(DOMAIN, self._config_entry.entry_id)},
+            name = "Hoymiles HMS-XXXXW-T2" + self._sensor_prefix,
+            manufacturer="Hoymiles",
+            serial_number= self._dtu_sn,
+            model="HMS-XXXXW-T2"
+        )
 
 
 class HoymilesCoordinatorEntity(CoordinatorEntity, HoymilesEntity):
@@ -45,8 +41,10 @@ class HoymilesCoordinatorEntity(CoordinatorEntity, HoymilesEntity):
 
     def __init__(self, coordinator: HoymilesCoordinatorEntity, config_entry: ConfigEntry, description: EntityDescription):
         """Pass coordinator to CoordinatorEntity."""
-        HoymilesEntity.__init__(self, config_entry, description)
         CoordinatorEntity.__init__(self, coordinator)
-
         if self.coordinator is not None and hasattr(self.coordinator, "data"):
             self._dtu_sn = getattr(self.coordinator.data, "device_serial_number", "")
+        HoymilesEntity.__init__(self, config_entry, description)
+
+
+

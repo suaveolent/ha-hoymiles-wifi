32abfeb39223f2029abd5a662931777b40321a37
suaveolent <suaveolent@users.noreply.github.com>
2024-02-08 17:28:12 +0100

    Separate file for coordinator

--- custom_components/hoymiles_wifi/__init__.py
+++ custom_components/hoymiles_wifi/__init__.py
@@ -6,8 +6,6 @@ import logging
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_HOST, Platform
 from homeassistant.core import Config, HomeAssistant
-from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
-from hoymiles_wifi.inverter import Inverter
 
 from .const import (
     CONF_UPDATE_INTERVAL,
@@ -96,69 +94,4 @@ async def options_update_listener(hass: HomeAssistant, config_entry: ConfigEntry
     """Handle options update."""
     _LOGGER.error("Options update called!")
     await hass.config_entries.async_reload(config_entry.entry_id)
-
-
-class HoymilesCoordinatorEntity(DataUpdateCoordinator):
-    """Base coordinator entity for Hoymiles integration."""
-
-    def __init__(self, hass: HomeAssistant, inverter: Inverter, entry: ConfigEntry, update_interval: timedelta) -> None:
-        """Initialize the HoymilesCoordinatorEntity."""
-        self._inverter = inverter
-        self._entities_added = False
-        self._hass = hass
-        self._entry = entry
-
-        _LOGGER.debug(
-            "Setup entry with update interval %s. IP: %s",
-            update_interval,
-            entry.data.get(CONF_HOST),
-        )
-
-        super().__init__(hass, _LOGGER, name=DOMAIN, update_interval=update_interval)
-
-    def get_inverter(self):
-        """Get the inverter object."""
-        return self._inverter
-
-
-
-class HoymilesDataUpdateCoordinatorInverter(HoymilesCoordinatorEntity):
-    """Data coordinator entity for Hoymiles integration."""
-
-    async def _async_update_data(self):
-        """Update data via library."""
-        _LOGGER.debug("Hoymiles data coordinator update")
-
-        response = self._inverter.get_real_data_new()
-
-        if not self._entities_added:
-            self._hass.async_add_job(
-                self._hass.config_entries.async_forward_entry_setups(self._entry, PLATFORMS)
-            )
-            self._entities_added = True
-
-        if response:
-            return response
-        else:
-            _LOGGER.debug("Unable to retrieve real data new. Inverter might be offline.")
-            return None
-
-
-
-class HoymilesConfigUpdateCoordinatorInverter(HoymilesCoordinatorEntity):
-    """Config coordinator entity for Hoymiles integration."""
-
-    async def _async_update_data(self):
-        """Update data via library."""
-        _LOGGER.debug("Hoymiles data coordinator update")
-
-        response = self._inverter.get_config()
-
-        if response:
-            return response
-        else:
-            _LOGGER.debug("Unable to retrieve real data new. Inverter might be offline.")
-            return None
-
-
 
---
+++ custom_components/hoymiles_wifi/coordinator.py
@@ -0,0 +1,77 @@
+""""Coordinator for Hoymiles integration."""
+from datetime import timedelta
+import logging
+
+import homeassistant
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import CONF_HOST, Platform
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
+from hoymiles_wifi.inverter import Inverter
+
+from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+
+PLATFORMS = [Platform.SENSOR, Platform.NUMBER, Platform.BINARY_SENSOR, Platform.BUTTON]
+
+class HoymilesCoordinatorEntity(DataUpdateCoordinator):
+    """Base coordinator entity for Hoymiles integration."""
+
+    def __init__(self, hass: homeassistant, inverter: Inverter, entry: ConfigEntry, update_interval: timedelta) -> None:
+        """Initialize the HoymilesCoordinatorEntity."""
+        self._inverter = inverter
+        self._entities_added = False
+        self._hass = hass
+        self._entry = entry
+
+        _LOGGER.debug(
+            "Setup entry with update interval %s. IP: %s",
+            update_interval,
+            entry.data.get(CONF_HOST),
+        )
+
+        super().__init__(hass, _LOGGER, name=DOMAIN, update_interval=update_interval)
+
+    def get_inverter(self):
+        """Get the inverter object."""
+        return self._inverter
+
+class HoymilesDataUpdateCoordinatorInverter(HoymilesCoordinatorEntity):
+    """Data coordinator entity for Hoymiles integration."""
+
+    async def _async_update_data(self):
+        """Update data via library."""
+        _LOGGER.debug("Hoymiles data coordinator update")
+
+        response = self._inverter.get_real_data_new()
+
+        if not self._entities_added:
+            self._hass.async_add_job(
+                self._hass.config_entries.async_forward_entry_setups(self._entry, PLATFORMS)
+            )
+            self._entities_added = True
+
+        if response:
+            return response
+        else:
+            _LOGGER.debug("Unable to retrieve real data new. Inverter might be offline.")
+            return None
+
+
+
+class HoymilesConfigUpdateCoordinatorInverter(HoymilesCoordinatorEntity):
+    """Config coordinator entity for Hoymiles integration."""
+
+    async def _async_update_data(self):
+        """Update data via library."""
+        _LOGGER.debug("Hoymiles data coordinator update")
+
+        response = self._inverter.get_config()
+
+        if response:
+            return response
+        else:
+            _LOGGER.debug("Unable to retrieve real data new. Inverter might be offline.")
+            return None
+
+
